35981acf0ea12e49f03af64cf3b22db9dc8d3cc8
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-03-17 19:49:06 +0000

    added edit player route

--- packages/frontend/app/routes.ts
+++ packages/frontend/app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   route("dashboard", "routes/dashboard.tsx", [
     route("player/:playerName", "routes/player.tsx"),
-    // route("player/:playerName/edit", "routes/player-edit.tsx"),
+    route("player/:playerName/edit", "routes/player-edit.tsx"),
   ]),
 ] satisfies RouteConfig;

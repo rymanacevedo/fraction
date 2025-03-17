b7c97572854fb3294009c5d5c29f35a69c01b9d8
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-03-17 16:41:22 +0000

    ran create router template for netlify

---
+++ packages/frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
+  server: {
+    allowedHosts: true,
+  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+}));
